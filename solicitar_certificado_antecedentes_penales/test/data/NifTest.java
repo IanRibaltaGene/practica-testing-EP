c97e3041554c1f021535f1db8d30ea7fb6fadbde
IanRibaltaGene <ian.ribalta@gmail.com>
2022-12-29 12:40:53 +0100

    Data package structure

---
+++ solicitar_certificado_antecedentes_penales/test/data/NifTest.java
@@ -0,0 +1,30 @@
+package data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NifTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNif() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
