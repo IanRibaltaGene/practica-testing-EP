3880e31f99a0f3894b63a0d878d1f1b8cbc904b3
IanRibaltaGene <ian.ribalta@gmail.com>
2022-12-31 11:57:42 +0100

    DocPath test done.

--- solicitar_certificado_antecedentes_penales/test/data/DocPathTest.java
+++ solicitar_certificado_antecedentes_penales/test/data/DocPathTest.java
@@ -1,34 +1,41 @@
 package data;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import exception.NullPathException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DocPathTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    private static DocPath pathOneChar;
+    private static DocPath pathMultipleChar;
 
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void getDocPath() {
+    @BeforeAll
+    static void setUp() throws NullPathException {
+        pathOneChar = new DocPath("a");
+        pathMultipleChar = new DocPath("a1a/sw1");
     }
 
     @Test
-    void testEquals() {
+    void constructorNullDocPath(){
+        Throwable nullException = assertThrows(NullPathException.class,
+                () -> {
+                    DocPath path = new DocPath(null);
+                });
+
+        assertEquals("Document path parameter cannot be null", nullException.getMessage());
     }
 
     @Test
-    void testHashCode() {
+    void getDocPathOneChar() {
+        assertEquals("a", pathOneChar.getDocPath());
+        assertNotEquals("a/", pathOneChar.getDocPath());
     }
 
     @Test
-    void testToString() {
+    void getDocPathMultipleChar() {
+        assertEquals("a1a/sw1", pathMultipleChar.getDocPath());
+        assertNotEquals("a", pathMultipleChar.getDocPath());
     }
 }
