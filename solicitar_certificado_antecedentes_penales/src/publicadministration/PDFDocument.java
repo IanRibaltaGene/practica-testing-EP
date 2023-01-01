775cec595f287f42372fdf31c6552b87e5f8fb91
IanRibaltaGene <ian.ribalta@gmail.com>
2023-01-01 21:53:11 +0100

    PDFDumen test + PDFDocumen class

--- solicitar_certificado_antecedentes_penales/src/publicadministration/PDFDocument.java
+++ solicitar_certificado_antecedentes_penales/src/publicadministration/PDFDocument.java
@@ -1,26 +1,62 @@
 package publicadministration;
 
 import data.DocPath;
+import exception.NullPathException;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
 public class PDFDocument {// Represents a PDF document
     private Date creatDate;
-    private DocPath path;
+    private DocPath documentPath;
     private File file;
 
-    public PDFDocument () {
-        //TODO. . .
+    public PDFDocument () throws NullPathException {
+        creatDate = new Date();
+        file = new File("./default_name.txt");
+        documentPath = new DocPath(file.getPath());
+        System.out.println("Document downloaded at the default path");
     } // Initializes attributes and emulates the document download at a default path
 
-    //TODO(. . .) // the getters
-    public String toString () {
-        //TODO. . .
-        return null;
+    // the getters
+    public Date getCreatDate() {
+        return creatDate;
+    }
+
+    public DocPath getDocumentPath() {
+        return documentPath;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    @Override
+    public String toString() {
+        return "PDFDocument{" +
+                "creatDate=" + creatDate +
+                ", path=" + documentPath +
+                '}';
     }// Converts to String members Date & DocPath
+
     // To implement only optionally
-    //TODO public void moveDoc (DocPath destPath) throws IOException; // Movesthe document to the destination path indicated
-    //TODO public void openDoc (DocPath path) throws IOException; // Opens the document at the path indicated
+    public void moveDoc (DocPath destPath) throws IOException{
+        this.documentPath = destPath;
+        File newDestination = new File(documentPath.getDocPath());
+        if (file.renameTo(newDestination)) {
+            System.out.println("The document has been moved successfully");
+        } else {
+            System.out.println("An error has occurred while moving the document");
+        }
+    } // Moves the document to the destination path indicated
+    public void openDoc (DocPath path) throws IOException{
+        try {
+            File fileToOpen = new File(path.getDocPath());
+            Desktop.getDesktop().open(fileToOpen);
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    } // Opens the document at the path indicated
 }
