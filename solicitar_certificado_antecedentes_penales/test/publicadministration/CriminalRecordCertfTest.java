506050164d27c1b3768f21e9834f23b927043f80
IanRibaltaGene <ian.ribalta@gmail.com>
2023-01-01 22:05:11 +0100

    CriminalRecordCertf class + test done, public administration done.

--- solicitar_certificado_antecedentes_penales/test/publicadministration/CriminalRecordCertfTest.java
+++ solicitar_certificado_antecedentes_penales/test/publicadministration/CriminalRecordCertfTest.java
@@ -1,22 +1,66 @@
 package publicadministration;
 
+import data.DigitalSignature;
+import data.Goal;
+import data.Nif;
+import data.goalTypes;
+import exception.*;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CriminalRecordCertfTest {
 
-    @BeforeEach
-    void setUp() {
+    private static Nif nif;
+    private static String name;
+    private static Goal goal;
+    private static DigitalSignature digSign;
+    private static CrimConvictionsColl crimConvs;
+
+    private static CriminalRecordCertf criminalRecordCertf;
+
+    @BeforeAll
+    static void setUp() throws WrongFormatNifException, NullNifException, NullSignatureException, NullPathException, NullGoalException {
+        nif = new Nif("32181154Z");
+        name = "Joe Doe";
+        goal = new Goal(goalTypes.GAMESECTOR);
+        byte[] byteArray = "asdasd".getBytes();
+        digSign = new DigitalSignature(byteArray);
+        CrimConviction crimConv = new CrimConviction(new Date(), "offense 1","sentence 1");
+        crimConvs = new CrimConvictionsColl();
+        crimConvs.addCriminalConviction(crimConv);
+        criminalRecordCertf = new CriminalRecordCertf(nif,
+                name, goal, digSign, crimConvs);
     }
 
-    @AfterEach
-    void tearDown() {
+
+    @Test
+    void getNif() {
+        assertEquals(nif, criminalRecordCertf.getNif());
     }
 
     @Test
-    void testToString() {
+    void getName() {
+        assertEquals(name, criminalRecordCertf.getName());
+    }
+
+    @Test
+    void getGoal() {
+        assertEquals(goal, criminalRecordCertf.getGoal());
+    }
+
+    @Test
+    void getDigSign() {
+        assertEquals(digSign, criminalRecordCertf.getDigSign());
+    }
+
+    @Test
+    void getCrimConvs() {
+        assertEquals(crimConvs, criminalRecordCertf.getCrimConvs());
     }
 }
--- solicitar_certificado_antecedentes_penales/test/publicadministration/PDFDocumentTest.java
+++ solicitar_certificado_antecedentes_penales/test/publicadministration/PDFDocumentTest.java
@@ -3,7 +3,6 @@ package publicadministration;
 import data.DocPath;
 import exception.NullPathException;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
